22725e1afae4456abf74f08992bfc8ebfdcadeba
seita_matsumoto <gyabooo@gmail.com>
2020-02-07 13:13:47 +0900

    一旦枠を作成

---
+++ pages/phase1.vue
@@ -0,0 +1,136 @@
+<template>
+  <section class="p-phase1">
+    <div class="p-top">
+      <h2 class="p-top__title">仮）学生による学生のための就活イベント！</h2>
+      <h3 class="p-top__date">2020年6月　開催！</h3>
+    </div>
+    <div class="p-about">
+      <h1 class="p-about__title">About</h1>
+      <h1 class="p-about__title">Find Web/Tech Job</h1>
+      <h1 class="p-about__title">Expo Osaka</h1>
+      <h1 class="p-about__title">2020</h1>
+      <div class="p-about__desc">
+        説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。
+        説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。
+        説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。
+        説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。
+      </div>
+    </div>
+    <div class="p-recruit">
+      <h1 class="p-recruit__title">学生スタッフ募集中！</h1>
+      <div class="p-recruit__desc">
+        説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。
+        説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。
+        説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。
+        説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。
+      </div>
+      <div class="p-recruit__btn btn-primary rounded-pill">応募はこちらから</div>
+    </div>
+    <div class="p-sponsor">
+      <h1 class="p-sponsor__title">スポンサー募集中！</h1>
+      <div class="p-sponsor__desc">
+        説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。
+        説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。
+        説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。
+        説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。説明文が入るよ。
+      </div>
+      <div class="p-sponsor__btn btn-primary rounded-pill">応募はこちらから</div>
+    </div>
+    <div class="p-scroll">
+      <span>トップへ戻る</span>
+    </div>
+    <div class="p-footer">
+      <div class="p-sns">
+        <div class="p-sns__twitter">Twitterはこちら</div>
+        <div class="p-sns__instagram">instagramはこちら</div>
+        <div class="p-sns__blog">公式ブログはこちら</div>
+        <div class="p-sns__connpass">connpassはこちら</div>
+      </div>
+      <div class="p-footer__text">Find Web/Tech Job</div>
+      <div class="p-footer__text">Expo Osaka</div>
+      <div class="p-footer__text">2020</div>
+    </div>
+  </section>
+
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$site_color: rgb(5, 62, 160);
+
+.p-phase1 {
+}
+.p-top {
+  padding: 80% 50px 0 50px;
+  &__title {
+    display: inline-block;
+    font-size: 20px;
+    letter-spacing: 1.3pt;
+  }
+  &__date {
+    display: inline-block;
+    font-size: 20px;
+    letter-spacing: 1.3pt;
+    padding-bottom: 11px;
+  }
+}
+.p-about {
+  padding: 80px 30px;
+  &__title {
+    font-size: 24px;
+  }
+  &__desc {
+    font-size: 14px;
+  }
+}
+.p-recruit {
+  padding: 80px 30px;
+  width: 100%;
+  &__title {
+    font-size: 24px;
+  }
+  &__desc {
+    font-size: 14px;
+    margin-bottom: 20px;
+  }
+  &__btn {
+    text-align: center;
+    width: 70%;
+    font-size: 14px;
+    margin: 0 auto;
+    padding: 15px 40px;
+    background-color: rgb(5, 62, 160);
+  }
+}
+.p-sponsor {
+  padding: 80px 30px;
+  width: 100%;
+  &__title {
+    font-size: 24px;
+  }
+  &__desc {
+    font-size: 14px;
+    margin-bottom: 20px;
+  }
+  &__btn {
+    text-align: center;
+    width: 70%;
+    font-size: 14px;
+    margin: 0 auto;
+    padding: 15px 40px;
+    background-color: $site_color;
+  }
+}
+.p-scroll {
+  color: $site_color;
+}
+.p-footer {
+  background-color: $site_color;
+}
+</style>
